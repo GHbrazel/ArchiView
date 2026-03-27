2575e156861c2d30296f7c616e2f9440dc757005
GHBrazel <nofloss@gmx.de>
2026-03-27 10:07:30 +0100

    #1 - Refactor extension command

--- src/extension.ts
+++ src/extension.ts
@@ -5,7 +5,6 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "ArchiView" is now active!');
@@ -13,14 +12,15 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('ArchiView.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('ArchiView', () => {
 		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ArchiView!');
+		vscode.commands.executeCommand('archi-view-sidebar.focus');
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+	console.log('Your extension "ArchiView" is now inactive. Goodbye!');
+}
